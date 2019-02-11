4101022282ba5a3d01be60dea3a9e2a623a3ec22
Konstantin <t676526253@gmail.com>
2019-02-11 14:14:31 +0200

    add

--- WebApplication1/WebApplication1/Controllers/VideosController.cs
+++ WebApplication1/WebApplication1/Controllers/VideosController.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web.Hosting;
 using System.Web.Http;
 using WebApplication1.Models;
 
@@ -12,17 +16,133 @@ namespace WebApplication1.Controllers
 {
     public class VideosController : ApiController
     {
+
+
+
+        // GET api/<controller>
         public HttpResponseMessage Get(string filename, string ext)
         {
-            var video = new VideoStream(filename, ext);
+            if (filename == null)
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+            
+            string filePath = HostingEnvironment.MapPath("~/Videos/") + filename + "." + ext;
+            //Request.Headers.Range = new RangeHeaderValue(0, 5000000);
+            //Request.Headers.Range = new RangeHeaderValue(5000000, 10000000);
+            if (Request.Headers.Range != null)
+            {
+                try
+                {
+                    
+                    System.Text.Encoder stringEncoder = Encoding.UTF8.GetEncoder();
+                    byte[] stringBytes = new byte[stringEncoder.GetByteCount(filePath.ToCharArray(), 0, filePath.Length, true)];
+                    stringEncoder.GetBytes(filePath.ToCharArray(), 0, filePath.Length, stringBytes, 0, true);
+                    MD5CryptoServiceProvider MD5Enc = new MD5CryptoServiceProvider();
+                    string hash = BitConverter.ToString(MD5Enc.ComputeHash(stringBytes)).Replace("-", string.Empty);
 
-            var response = Request.CreateResponse(); // Stream outputStream, HttpContent content, TransportContext context
+                    HttpResponseMessage partialResponse = Request.CreateResponse(HttpStatusCode.PartialContent);
+                    partialResponse.Headers.AcceptRanges.Add("bytes");
+                    partialResponse.Headers.ETag = new EntityTagHeaderValue("\"" + hash + "\"");
 
-            Action<Stream, HttpContent, TransportContext> ww = video.WriteToStream;
-            response.Content = new PushStreamContent(ww, new MediaTypeHeaderValue("video/" + ext));
+                    var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+                    long ee = stream.Length;//451469775
 
-            return response;
+                    
+                    //Request.Headers.Range = new RangeHeaderValue(0 , stream.Length);
+ 
+                    partialResponse.Content = new ByteRangeStreamContent(stream, Request.Headers.Range, new MediaTypeHeaderValue("video/mp4"));
+
+                    return partialResponse;
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                    return new HttpResponseMessage(HttpStatusCode.InternalServerError);
+                }
+            }
+            else
+            {
+                return new HttpResponseMessage(HttpStatusCode.RequestedRangeNotSatisfiable);
+            }
         }
-
     }
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
+
+
+
+
+
+//public HttpResponseMessage Get(string filename, string ext)
+//{
+//    var video = new VideoStream(filename, ext);
+
+//    var response = Request.CreateResponse(); // Stream outputStream, HttpContent content, TransportContext context
+
+//    Action<Stream, HttpContent, TransportContext> ww = video.WriteToStream;
+//    response.Content = new PushStreamContent(ww, new MediaTypeHeaderValue("video/" + ext));
+
+//    return response;
+//}
+
+//    }
+
+
+
+
+
+
+
+//public class VideoController : ApiController
+//{
+
+//    // GET api/<controller>
+//    public HttpResponseMessage Get(string filename)
+//    {
+//        if (filename == null)
+//            return new HttpResponseMessage(HttpStatusCode.BadRequest);
+
+//        string filePath = HostingEnvironment.MapPath("~/Videos/") + filename;
+
+//        if (Request.Headers.Range != null)
+//        {
+//            //Range Specifc request: Stream video on wanted range.
+//            try
+//            {
+//                //NOTE: ETag calculation only with file name is one approach (Not the best one though - GUIDs or DateTime is may required in live applications.).
+//                Encoder stringEncoder = Encoding.UTF8.GetEncoder();
+//                byte[] stringBytes = new byte[stringEncoder.GetByteCount(filePath.ToCharArray(), 0, filePath.Length, true)];
+//                stringEncoder.GetBytes(filePath.ToCharArray(), 0, filePath.Length, stringBytes, 0, true);
+//                MD5CryptoServiceProvider MD5Enc = new MD5CryptoServiceProvider();
+//                string hash = BitConverter.ToString(MD5Enc.ComputeHash(stringBytes)).Replace("-", string.Empty);
+
+//                HttpResponseMessage partialResponse = Request.CreateResponse(HttpStatusCode.PartialContent);
+//                partialResponse.Headers.AcceptRanges.Add("bytes");
+//                partialResponse.Headers.ETag = new EntityTagHeaderValue("\"" + hash + "\"");
+
+//                var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+//                partialResponse.Content = new ByteRangeStreamContent(stream, Request.Headers.Range, new MediaTypeHeaderValue("video/mp4"));
+//                return partialResponse;
+//            }
+//            catch (Exception ex)
+//            {
+//                return new HttpResponseMessage(HttpStatusCode.InternalServerError);
+//            }
+//        }
+//        else
+//        {
+//            return new HttpResponseMessage(HttpStatusCode.RequestedRangeNotSatisfiable);
+//        }
+//    }
+//}
--- WebApplication1/WebApplication1/Models/VideoStream.cs
+++ WebApplication1/WebApplication1/Models/VideoStream.cs
@@ -21,16 +21,18 @@ namespace WebApplication1.Models
         {
             try
             {
-                var buffer = new byte[65536];
+                byte[] buffer = new byte[65536];
 
                 using (var video = File.Open(_filename, FileMode.Open, FileAccess.Read))
                 {
+
                     var length = (int)video.Length;
                     var bytesRead = 1;
 
                     while (length > 0 && bytesRead > 0)
                     {
                         bytesRead = video.Read(buffer, 0, Math.Min(length, buffer.Length));
+
                         await outputStream.WriteAsync(buffer, 0, bytesRead);
                         length -= bytesRead;
                     }
@@ -47,3 +49,53 @@ namespace WebApplication1.Models
         }
     }
 }
+
+
+
+
+
+
+
+
+//public class VideoController : ApiController
+//{
+
+//    // GET api/<controller>
+//    public HttpResponseMessage Get(string filename)
+//    {
+//        if (filename == null)
+//            return new HttpResponseMessage(HttpStatusCode.BadRequest);
+
+//        string filePath = HostingEnvironment.MapPath("~/Videos/") + filename;
+
+//        if (Request.Headers.Range != null)
+//        {
+//            //Range Specifc request: Stream video on wanted range.
+//            try
+//            {
+//                //NOTE: ETag calculation only with file name is one approach (Not the best one though - GUIDs or DateTime is may required in live applications.).
+//                Encoder stringEncoder = Encoding.UTF8.GetEncoder();
+//                byte[] stringBytes = new byte[stringEncoder.GetByteCount(filePath.ToCharArray(), 0, filePath.Length, true)];
+//                stringEncoder.GetBytes(filePath.ToCharArray(), 0, filePath.Length, stringBytes, 0, true);
+//                MD5CryptoServiceProvider MD5Enc = new MD5CryptoServiceProvider();
+//                string hash = BitConverter.ToString(MD5Enc.ComputeHash(stringBytes)).Replace("-", string.Empty);
+
+//                HttpResponseMessage partialResponse = Request.CreateResponse(HttpStatusCode.PartialContent);
+//                partialResponse.Headers.AcceptRanges.Add("bytes");
+//                partialResponse.Headers.ETag = new EntityTagHeaderValue("\"" + hash + "\"");
+
+//                var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+//                partialResponse.Content = new ByteRangeStreamContent(stream, Request.Headers.Range, new MediaTypeHeaderValue("video/mp4"));
+//                return partialResponse;
+//            }
+//            catch (Exception ex)
+//            {
+//                return new HttpResponseMessage(HttpStatusCode.InternalServerError);
+//            }
+//        }
+//        else
+//        {
+//            return new HttpResponseMessage(HttpStatusCode.RequestedRangeNotSatisfiable);
+//        }
+//    }
+//}
